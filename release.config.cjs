97c0c41314bb95f1a7c4b533b3e41ddf3b246c51
Your Name <camilo.barros.c@gmail.com>
2026-04-02 17:33:30 -0300

    fix: release from protected branches without pushback

--- release.config.cjs
+++ release.config.cjs
@@ -3,25 +3,11 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: false,
-      },
-    ],
     [
       '@semantic-release/github',
       {
         successComment: false,
         failComment: false,
-      },
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],
